914b617f8c6d1883df97e5aba33c45f9ac2b7bd3
AlexSki11 <sasha.morozov022@yandex.ru>
2025-01-03 09:38:36 +0300

    add 2 task

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let even = 0
 let zero = 0
 let odd = 0
 
-function check_number (arr) {
+function check_arr (arr) {
 
 	for (item of arr){
 
@@ -30,10 +30,60 @@ function check_number (arr) {
 	
 }
 
-check_number(arr)
-
+console.log(arr)
+check_arr(arr)
 console.log('Четных ', even)
 console.log('Нечетных ', odd)
 console.log('Нулей', zero)
+/*
+let n = 'y'
+arr = []
+
+even = 0
+odd = 0
+zero = 0
+
+
+while (n=='y') {
+
+	arr+=(prompt('Введите число для массива').split(' '))
+	n = prompt('y - продолжаем? n - нет')
+
+}
+
+console.log(arr)
+check_arr(arr)
+console.log('Четных ', even)
+console.log('Нечетных ', odd)
+console.log('Нулей', zero)
+*/
+
 
 //Задание 2
+function task2 (number) {
+	if (number > 1000){
+		alert('Нельзя больше 1т')
+		n = new Number(prompt('Введите число не больше 1т'))
+		return task2(n)
+	}
+
+	if (number <= 1){
+		return false
+	}
+
+	for(var i = 2; i <= number; i++){
+		if (number % i == 0 && i != number) {
+
+			return false
+
+		}
+
+	}
+
+	return true
+
+}
+
+n = prompt("Введите число для 2 задания")
+
+console.log(task2(n))
